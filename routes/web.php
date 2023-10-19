2a31bff70f1784c8f4981680d0e6cd53671e394a
Yasin Korkmaz <theacherx@gmail.com>
2023-10-20 01:15:51 +0300

    Map + Implode

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $product = \App\Models\Product::with('subProducts')->first();
+    $subProductsList = $product->subProducts
+        ->map(fn($subProduct) => $subProduct->name)
+        ->implode('<br>');
+
+    return $subProductsList;
 });
